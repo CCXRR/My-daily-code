da24b040aafd68b9834f19f04ff65135c742e424
CCXRR <965440313@qq.com>
2021-08-20 18:13:52 +0800

    change

---
+++ recursion/递归/recursion.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef _RECURSION_H_
+#define _RECURSION_H_
+
+void print(int x);
+int MAX(int arr1[], int n);
+int my_strlen(char* p);
+int Fac(int n);
+int Fib(int n);
+void Hanoi(int layer, char A, char B, char C, int* count);
+int Frog(int step);
+
+#endif
