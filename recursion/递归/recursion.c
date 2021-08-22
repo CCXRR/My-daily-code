fdc28715ff05ce7a3dd29f71a4cf2ac1c1c87bad
CCXRR <965440313@qq.com>
2021-08-22 09:30:31 +0800

    recursion

--- recursion/递归/recursion.c
+++ recursion/递归/recursion.c
@@ -12,67 +12,67 @@
 
 int main()
 {
-	//{//1. 接受一个整型值（无符号），按照顺序打印它的每一位。例如：输入：1234，输出 1 2 3 4.
-	//	int a;
+	{//1. 接受一个整型值（无符号），按照顺序打印它的每一位。例如：输入：1234，输出 1 2 3 4.
+		int a;
 
-	//	scanf("%d", &a);
-	//	print(a);
+		scanf("%d", &a);
+		print(a);
 
-	//	return 0;
-	//}
+		return 0;
+	}
 
-	//{//2. 求数组前n项的最大值
-	//				 0  1  2  3  4   5   6   7   8 
-	//	int arr1[] = { 8, 9, 1, 3, 23, 15, 16, 27, 33 }; //array	数组
-	//	int n;
-	//	int max;
-	//	scanf("%d", &n);
-	//	max = max(arr1, n);
-	//	printf("最大值为:>%d\n", max);
-	//	return 0;
-	//}
+	{//2. 求数组前n项的最大值
+					 0  1  2  3  4   5   6   7   8 
+		int arr1[] = { 8, 9, 1, 3, 23, 15, 16, 27, 33 }; //array	数组
+		int n;
+		int max;
+		scanf("%d", &n);
+		max = max(arr1, n);
+		printf("最大值为:>%d\n", max);
+		return 0;
+	}
 
-	//{//3. 编写函数不允许创建临时变量，求字符串长度
-	//	int length;
-	//	char arr1[] = "interesting";
+	{//3. 编写函数不允许创建临时变量，求字符串长度
+		int length;
+		char arr1[] = "interesting";
 
-	//	length = my_strlen(arr1);
-	//	printf("%d\n", length);
-	//	return 0;
-	//}
+		length = my_strlen(arr1);
+		printf("%d\n", length);
+		return 0;
+	}
 
-	//{//4. 求n的阶乘。（不考虑溢出）
-	//	int n;
-	//	int fac;				//factorial		阶乘
+	{//4. 求n的阶乘。（不考虑溢出）
+		int n;
+		int fac;				//factorial		阶乘
 
-	//	scanf("%d", &n);
-	//	fac = fac(n);
-	//	printf("%d\n", fac);
+		scanf("%d", &n);
+		fac = fac(n);
+		printf("%d\n", fac);
 
-	//	return 0;
-	//}
+		return 0;
+	}
 
-	//{//5. 求斐波那契数(fibonacci)（不考虑溢出）
-	//	int n;
-	//	int fib;
+	{//5. 求斐波那契数(fibonacci)（不考虑溢出）
+		int n;
+		int fib;
 
-	//	scanf("%d", &n);
-	//	fib = fib(n);
-	//	printf("%d\n", fib);
+		scanf("%d", &n);
+		fib = fib(n);
+		printf("%d\n", fib);
 
-	//	return 0;
-	//}
+		return 0;
+	}
 
-	//{//6. 求汉诺塔（Tower of Hanoi）
-	//	int layer;
-	//	int count = 0;
-	//	
-	//	scanf("%d", &layer);
-	//	Hanoi(layer, 'A', 'B', 'C', &count);
-	//	printf("共移动%d次\n", count);
+	{//6. 求汉诺塔（Tower of Hanoi）
+		int layer;
+		int count = 0;
+		
+		scanf("%d", &layer);
+		Hanoi(layer, 'A', 'B', 'C', &count);
+		printf("共移动%d次\n", count);
 
-	//	return 0;
-	//}
+		return 0;
+	}
 
 	{//7. 青蛙跳台阶
 		int step;
